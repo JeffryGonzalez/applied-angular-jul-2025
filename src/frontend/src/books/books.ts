399cf075b208512560e48159fb7894bf3bd31e27
Jamie M <7iny.d4nc3r@gmail.com>
2025-07-24 19:21:50 +0000

    step 1 - show a list of books and display id, title, author, and year

--- src/frontend/src/books/books.ts
+++ src/frontend/src/books/books.ts
@@ -1,12 +1,32 @@
-import { JsonPipe } from '@angular/common';
 import { Component, ChangeDetectionStrategy, resource } from '@angular/core';
 import { BookApiItem } from './types';
 
 @Component({
   selector: 'app-books',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [JsonPipe],
-  template: ` <pre>{{ booksResource.value() | json }}</pre> `,
+  imports: [],
+  template: `
+    <ul class="list bg-base-100 rounded-box shadow-md">
+      @let books = booksResource.value();
+      <!-- <li class="p-4 pb-2 text-xs opacity-60 tracking-wide">
+      Most played songs this week
+    </li> -->
+      @for (book of books; track book.id) {
+        <li class="list-row">
+          <div>
+            <div>
+              {{ book.id }}: <span class="font-bold">{{ book.title }}</span> ({{
+                book.year
+              }})
+            </div>
+            <div class="text-xs uppercase font-semibold opacity-60">
+              {{ book.author }}
+            </div>
+          </div>
+        </li>
+      }
+    </ul>
+  `,
   styles: ``,
 })
 export class Books {
